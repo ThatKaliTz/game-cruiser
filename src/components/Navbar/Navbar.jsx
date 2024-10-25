272d0055a053ecf7c53c912f8f8a27a9e91193dc
Rogelio Martinez <127463454+ThatKaliTz@users.noreply.github.com>
2024-10-25 13:39:06 -0600

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
                                 <input type="search" placeholder="Search games.." className="bg-gray-700/50 px-4 py-2 rounded-2xl" />
                             </li>
                             <li>
-                            <Link to="/user" className="cursor-pointer">User
+                            <Link to="/user" className="cursor-pointer">
                                     {loggedInUser ? `Hello, ${loggedInUser}` : "User"}
                                     </Link>
                             </li>
